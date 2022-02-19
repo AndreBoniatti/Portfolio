5759e2255adbff2e628b4b91df1234d1cc9fc13e
André Boniatti <andre123boniatti@gmail.com>
2022-02-19 11:46:27 -0300

    feat: finalizado sidenav

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css'],
 })
-export class SidenavComponent {}
+export class SidenavComponent {
+  @ViewChild(MatSidenav) sidenav: MatSidenav;
+
+  constructor(private observer: BreakpointObserver) {}
+
+  ngAfterViewInit() {
+    this.observer
+      .observe(['(max-width: 800px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
+  }
+}
