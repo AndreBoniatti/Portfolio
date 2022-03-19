67ce5c13ca8612f8117a53849e2a2c29dbfff88e
André Boniatti <andre123boniatti@gmail.com>
2022-03-19 11:56:48 -0300

    feat: testando comunicacao com o backend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
 import { SidenavService } from './../shared/components/sidenav/sidenav.service';
 
 @Component({
@@ -8,11 +10,25 @@ import { SidenavService } from './../shared/components/sidenav/sidenav.service';
 export class HomeComponent implements OnInit {
   currentPage: string = 'home';
 
-  constructor(private sidenavService: SidenavService) {}
+  constructor(
+    private sidenavService: SidenavService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
     this.sidenavService.getCurrentPage().subscribe((res) => {
       if (res) this.currentPage = res;
     });
+  }
+
+  teste(): void {
+    this.http.post<any>('http://localhost:3000', {}).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 }
